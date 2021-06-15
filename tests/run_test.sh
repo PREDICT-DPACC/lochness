8f307fe41f503c0e1dcda179f38f34cd22d0de71
Kang Ik Kevin Cho <kevincho@bwh.harvard.edu>
2021-06-15 16:56:08 -0400

    feat: tree and mediaflux tests added

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -4,6 +4,8 @@ pytest lochness_test/daris/test_daris.py
 pytest lochness_test/redcap/test_redcap.py
 pytest lochness_test/keyring/test_keyring.py
 pytest lochness_test/mindlamp/test_mindlamp.py
+pytest lochness_test/tree/test_tree.py
+pytest lochness_test/tree/test_mediaflux.py
 
 pytest scripts_test/test_sync.py
 pytest scripts_test/test_lochness_create_template.py
